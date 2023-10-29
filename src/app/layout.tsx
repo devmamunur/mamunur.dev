2ed517b32849106e287c4bd23c2c6c488b3085c5
Mamunur Rashid <devmamunur1@gmail.com>
2023-10-29 12:34:53 +0600

    configure fontawesome and JetBrains_Mono web font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { JetBrains_Mono } from 'next/font/google'
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
-const inter = Inter({ subsets: ['latin'] })
+const jetBrains_Mono = JetBrains_Mono({
+  weight: ['300', '400', '500', '600', '700', '800'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={jetBrains_Mono.className}>{children}</body>
     </html>
   )
 }
