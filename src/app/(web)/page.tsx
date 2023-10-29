e215f7bc1e527950548f0fdb016b66d258036313
Mamunur Rashid <devmamunur1@gmail.com>
2023-10-29 12:36:10 +0600

    import and use navbar and hero-area component in main home page

---
+++ src/app/(web)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from "@/components/NavBar";
+import HeroArea from "@/components/HeroArea";
+
+const HomePage : React.FC = () => {
+    return (
+        <>
+            <NavBar/>
+            <HeroArea/>
+        </>
+    );
+};
+
+export default HomePage;
