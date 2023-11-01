56b1b552caf3f6b5e7f1c9fcfef559afb09314f6
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-01 07:40:52 +0600

    update StatisticCard

--- src/components/Statistic/StatisticCard.tsx
+++ src/components/Statistic/StatisticCard.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import Image from "next/image";
 
-const StatisticCard : React.FC = () => {
+const StatisticCard: React.FC = () => {
     return (
         <>
-            <div className="col-span-2 bg-white py-6 px-6 rounded-2xl dark:bg-gray-800">
-                <div className="text-center md:text-left">
-                    <h6 className="text-2xl mb-3 text-gray-50000 font-bold dark:text-gray-200">@devmamunur</h6>
-                    <h6 className="text-1xl mb-1 text-gray-100 dark:text-gray-200">Want To Know More</h6>
-                    <h6 className="text-2xl text-gray-100 font-bold uppercase dark:text-gray-200">Fun Fact</h6>
+            <div className="col-span-2 section-card-wrapper">
+                <div className="bg-gradient-overlay"></div>
+                <Image
+                    className="image-overlay"
+                    src='/bg1.png'
+                    width='100'
+                    height='100'
+                    priority
+                    alt='read more'
+                />
+                <div className="section-card">
+                    <ul className="flex justify-between align-middle text-center my-8">
+                        <li className="">
+                            <span>
+                                <h3 className="dark:text-gray-200 text-3xl mb-3">1500 +</h3>
+                                <p className="dark:text-gray-400 text-1xl">Product Sell</p>
+                            </span>
+                        </li>
+                        <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
+                            <span>
+                                <h3 className="dark:text-gray-200 text-3xl mb-3">100 +</h3>
+                                <p className="dark:text-gray-400 text-1xl">Product Made</p>
+                            </span>
+                        </li>
+                        <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
+                            <span>
+                                <h3 className="dark:text-gray-200 text-3xl mb-3">60 +</h3>
+                                <p className="dark:text-gray-400 text-1xl">5-Star Rating</p>
+                            </span>
+                        </li>
+                        <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
+                            <span>
+                                <h3 className="dark:text-gray-200 text-3xl mb-3">4y +</h3>
+                                <p className="dark:text-gray-400 text-1xl">Experience</p>
+                            </span>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </>
