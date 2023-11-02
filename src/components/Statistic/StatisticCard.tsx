ca227401fe1287cb1d1e9f66d41b5b17d4a425a5
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-02 11:23:13 +0600

    update StatisticCard card

--- src/components/Statistic/StatisticCard.tsx
+++ src/components/Statistic/StatisticCard.tsx
@@ -2,44 +2,39 @@ import React from 'react';
 import Image from "next/image";
 
 const StatisticCard: React.FC = () => {
+    const statistics = [
+        {
+            number : '1500 +',
+            label : 'Product Sell',
+        },
+        {
+            number : '100 +',
+            label : 'Product Made',
+        },
+        {
+            number : '80 +',
+            label : '5-Star Rating',
+        },
+        {
+            number : '4y +',
+            label : 'Experience',
+        }
+    ]
     return (
         <>
             <div className="col-span-2 section-card-wrapper">
-                <div className="bg-gradient-overlay"></div>
-                <Image
-                    className="image-overlay"
-                    src='/bg1.png'
-                    width='100'
-                    height='100'
-                    priority
-                    alt='read more'
-                />
-                <div className="section-card">
-                    <ul className="flex justify-between align-middle text-center my-8">
-                        <li className="">
-                            <span>
-                                <h3 className="dark:text-gray-200 text-3xl mb-3">1500 +</h3>
-                                <p className="dark:text-gray-400 text-1xl">Product Sell</p>
-                            </span>
-                        </li>
-                        <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
-                            <span>
-                                <h3 className="dark:text-gray-200 text-3xl mb-3">100 +</h3>
-                                <p className="dark:text-gray-400 text-1xl">Product Made</p>
-                            </span>
-                        </li>
-                        <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
-                            <span>
-                                <h3 className="dark:text-gray-200 text-3xl mb-3">60 +</h3>
-                                <p className="dark:text-gray-400 text-1xl">5-Star Rating</p>
-                            </span>
-                        </li>
-                        <li className="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5">
-                            <span>
-                                <h3 className="dark:text-gray-200 text-3xl mb-3">4y +</h3>
-                                <p className="dark:text-gray-400 text-1xl">Experience</p>
-                            </span>
-                        </li>
+                <div className="section-card h-full w-full flex items-center">
+                    <ul className="flex justify-between w-full align-middle text-center">
+                        {
+                            statistics.map((item, i) =>
+                                <li key={i}>
+                                    <span>
+                                        <h3 className="dark:text-gray-200 text-3xl mb-3">{item.number}</h3>
+                                        <p className="dark:text-gray-400 text-1xl">{item.label}</p>
+                                    </span>
+                                </li>
+                            )
+                        }
                     </ul>
                 </div>
             </div>
