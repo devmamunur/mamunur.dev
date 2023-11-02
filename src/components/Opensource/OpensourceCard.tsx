254c71a470b2bf3ed891220b9f25251f46db64af
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-02 11:23:36 +0600

    Update OpensourceCard

--- src/components/Opensource/OpensourceCard.tsx
+++ src/components/Opensource/OpensourceCard.tsx
@@ -5,16 +5,7 @@ import Link from "next/link";
 const OpensourceCard : React.FC = () => {
     return (
         <>
-            <div className="col-span-2 mt-6 section-card-wrapper">
-                <div className="bg-gradient-overlay"></div>
-                <Image
-                    className="image-overlay"
-                    src='/bg1.png'
-                    width='100'
-                    height='100'
-                    priority
-                    alt='read more'
-                />
+            <Link href={`/opensource-projects`} className="col-span-2 mt-6 section-card-wrapper">
                 <div className="group section-card">
                     <div className="flex justify-between">
                         <div>
@@ -25,9 +16,9 @@ const OpensourceCard : React.FC = () => {
                             <Image src='/icon.svg' width='40' height='40' priority alt='read more'/>
                         </Link>
                     </div>
-                    <h6 className="text-1xl font-bold text-sky-500">PMSGO, VueBusiness, Advanced-JavaScript-Guide, mamunur.dev</h6>
+                    <h6 className="text-1xl font-bold text-sky-500">PMSGO, Advanced-JavaScript-Guide, mamunur.dev, ...</h6>
                 </div>
-            </div>
+            </Link>
         </>
     );
 };
