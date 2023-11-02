21cf46cfd647f4f2cb530eab92e590185cac56c0
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-02 11:25:06 +0600

    Update ContactCard

--- src/components/Blog/BlogCard.tsx
+++ src/components/Blog/BlogCard.tsx
@@ -5,16 +5,7 @@ import Link from "next/link";
 const BlogCard : React.FC = () => {
     return (
         <>
-            <div className="col-span-2 section-card-wrapper">
-                <div className="bg-gradient-overlay"></div>
-                <Image
-                    className="image-overlay"
-                    src='/bg1.png'
-                    width='100'
-                    height='100'
-                    priority
-                    alt='read more'
-                />
+            <div className="col-span-1 section-card-wrapper">
                 <div className="group section-card">
                     <div className="flex justify-between">
                         <div>
