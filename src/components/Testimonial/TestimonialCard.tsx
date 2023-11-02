5bc07e7650dc3728fc60bdfe15b8b3cfe07cfb63
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-02 11:23:04 +0600

    update testimonial card

--- src/components/Testimonial/TestimonialCard.tsx
+++ src/components/Testimonial/TestimonialCard.tsx
@@ -5,17 +5,8 @@ import Link from "next/link";
 const TestimonialCard : React.FC = () => {
     return (
         <>
-            <div className="col-span-1 section-card-wrapper">
-                <div className="bg-gradient-overlay"></div>
-                <Image
-                    className="image-overlay"
-                    src='/bg1.png'
-                    width='100'
-                    height='100'
-                    priority
-                    alt='read more'
-                />
-                <div className="section-card text-bottom">
+            <div className="col-span-2 section-card-wrapper">
+                <div className="group section-card">
                     <div className="flex justify-between">
                         <div>
                             <h6 className="section-card-sub-title">Want Clients Say About Me</h6>
@@ -25,7 +16,7 @@ const TestimonialCard : React.FC = () => {
                             <Image src='/icon.svg' width='40' height='40' priority alt='read more'/>
                         </Link>
                     </div>
-                    <h6 className="text-1xl font-bold text-sky-500">@devmamunur</h6>
+                    <h6 className="text-1xl font-bold text-sky-500">Envato</h6>
                 </div>
             </div>
         </>
