77868c1aad6300db4fc8a9259688617e487d65e6
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-02 11:24:39 +0600

    Update footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,17 +6,8 @@ const Footer: React.FC = () => {
         <>
             <div
                 className="mx-auto max-w-screen-xl grid grid-cols-3 gap-6 flex my-6 items-center justify-center text-center">
-                <footer className="col-span-3 bg-white rounded-lg shadow p-5  dark:bg-gray-900 relative">
-                    <div className="bg-gradient-overlay"></div>
-                    <Image
-                        className="image-overlay"
-                        src='/bg1.png'
-                        width='100'
-                        height='100'
-                        priority
-                        alt='read more'
-                    />
-                    <span className="relative z-10 text-md text-gray-500 sm:text-center dark:text-gray-400">© 2023 <span
+                <footer className="col-span-3 p-5  bg-transparent relative">
+                    <span className=" text-md text-gray-500 sm:text-center dark:text-gray-400">© 2023 <span
                         className="text-blue-500">@devmamunur</span>. All Rights Reserved.
                     </span>
 
