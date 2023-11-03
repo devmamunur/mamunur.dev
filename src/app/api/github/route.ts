c19abc9f9bebc5915f5a693fc577005eb3d2e5ce
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-03 08:20:33 +0600

    made github route

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -1,1 +1,11 @@
-//test
+import {NextResponse} from "next/server";
+import getGitHubRepo from "@/services/github/getRepo";
+
+export async function GET(request: Request) {
+    try{
+        const gitRepos = await getGitHubRepo();
+        return NextResponse.json({success : true, data : gitRepos}, {status : 200})
+    }catch (error){
+        return NextResponse.json({success : false, data : ''}, {status : 500})
+    }
+}
