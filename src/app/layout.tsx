11961e86349982761675d2be27fa80050633ff9d
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-05 18:42:40 +0600

    update main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,7 @@ import './globals.css';
 import { JetBrains_Mono } from 'next/font/google';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
-import NavBar from '@/components/NavBar';
 import React from 'react';
-import Footer from '@/components/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -17,12 +15,6 @@ const jetBrains_Mono = JetBrains_Mono({
   display: 'swap',
 });
 
-export const metadata = {
-  title: '@devmamunur | Mamunur Rashid Software Developer',
-  description:
-    'I am Full Stack Web Developer | Open Source Enthusiast | JavaScript, TypeScript, Node.js, React.js, Vue.js | 4 years of industry experience.',
-};
-
 export default function RootLayout({
   children,
 }: {
@@ -31,9 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${jetBrains_Mono.className} bg-slate-900`}>
-        <NavBar />
         {children}
-        <Footer />
         <ToastContainer />
       </body>
     </html>
