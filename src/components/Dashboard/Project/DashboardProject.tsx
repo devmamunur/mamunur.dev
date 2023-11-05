624e67f029ef943974d1d90ea5814949b8d3b43e
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-05 19:10:48 +0600

    test changes

--- src/components/Dashboard/Project/DashboardProject.tsx
+++ src/components/Dashboard/Project/DashboardProject.tsx
@@ -6,7 +6,7 @@ const DashboardProject = () => {
       <div className="mx-auto max-w-screen-xl mt-6 grid grid-cols-1">
         <div className="col-span-1 section-card-wrapper">
           <div className="group section-card">
-            <span className="text-white">DashboardProject</span>
+            <span className="text-white"> DashboardProject </span>
           </div>
         </div>
       </div>
--- src/components/Dashboard/contribution/DashboardContribution.tsx
+++ src/components/Dashboard/contribution/DashboardContribution.tsx
@@ -6,7 +6,7 @@ const DashboardContribution = () => {
       <div className="mx-auto max-w-screen-xl mt-6 grid grid-cols-1">
         <div className="col-span-1 section-card-wrapper">
           <div className="group section-card">
-            <span className="text-white">DashboardContributions</span>
+            <span className="text-white"> DashboardContributions </span>
           </div>
         </div>
       </div>
