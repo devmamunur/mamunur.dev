23341a3ca99b5c59d28f3b38e89cd3583d506d2b
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-05 18:42:30 +0600

    made mongoose  configuration

---
+++ src/config/mongo.ts
@@ -0,0 +1,34 @@
+import { serverEnv } from '@/config/schemas/serverSchema';
+import mongoose, { connect, ConnectOptions } from 'mongoose';
+import logger from '@/config/logger';
+
+let connection: typeof mongoose | null;
+
+const connectMongo = async (): Promise<typeof mongoose | null> => {
+  const { DEVMAMUNUR_MONGO_CONNECTION_STRING } = serverEnv;
+
+  if (!DEVMAMUNUR_MONGO_CONNECTION_STRING) {
+    throw new Error(
+      'Please define the DEVMAMUNUR_MONGO_CONNECTION_STRING environment variable (if local add to .env file)'
+    );
+  }
+
+  if (connection) {
+    return connection;
+  }
+
+  try {
+    const options: ConnectOptions = {
+      autoIndex: true,
+      family: 4,
+      maxPoolSize: 10,
+    };
+    connection = await connect(DEVMAMUNUR_MONGO_CONNECTION_STRING, options);
+    logger.info('DB connection successful:', connection);
+    return connection;
+  } catch (error) {
+    const errorMessage = (error as Error).message;
+    logger.error('DB connection failed:', errorMessage);
+    throw error;
+  }
+};
