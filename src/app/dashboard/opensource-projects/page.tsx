63c5a98efa1967bf09e086d096a689ddb93dfe49
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-05 18:42:54 +0600

    made dashboard OpensourceProjectsPage

---
+++ src/app/dashboard/opensource-projects/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardProject from '@/components/Dashboard/Project/DashboardProject';
+
+const OpensourceProjectsPage = () => {
+  return (
+    <div>
+      <DashboardProject />
+    </div>
+  );
+};
+
+export default OpensourceProjectsPage;
