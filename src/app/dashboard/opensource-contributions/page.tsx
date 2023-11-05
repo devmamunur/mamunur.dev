ac0695644d7df4726a744f3e4737d87903cbe60e
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-05 18:43:04 +0600

    made dashboard OpensourceContributionsPage

---
+++ src/app/dashboard/opensource-contributions/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardContribution from '@/components/Dashboard/contribution/DashboardContribution';
+
+const OpensourceContributionsPage = () => {
+  return (
+    <>
+      <DashboardContribution />
+    </>
+  );
+};
+
+export default OpensourceContributionsPage;
