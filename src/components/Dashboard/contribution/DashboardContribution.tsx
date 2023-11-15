a976564f93000963f142f2a9841d2311db1f5b24
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-16 01:29:14 +0600

    add validation and show repo

--- src/components/Dashboard/contribution/DashboardContribution.tsx
+++ src/components/Dashboard/contribution/DashboardContribution.tsx
@@ -1,30 +1,39 @@
 'use client';
 import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { clientEnv } from '@/config/schemas/clientSchema';
-import { SetRepoData } from '@/interface/opensource';
+import { GitHubRepoData } from '@/interface/opensource';
+import ToastHelper from '@/helpers/toastHelper';
+import { z } from 'zod';
 
+const RepoAddSchema = z.object({
+  url: z.string().url(),
+});
 const DashboardContribution = () => {
   const [loader, setLoader] = useState<boolean>(false);
   const [output, setOutput] = useState<string>('');
-  const [repos, setRepos] = useState<SetRepoData[]>([]);
+  const [repos, setRepos] = useState<GitHubRepoData[]>([]);
   const [url, setURL] = useState<string>('');
 
-  const fetchData = async () => {
-    try {
-      const data = await fetch(`${clientEnv.NEXT_PUBLIC_BASE_URL}/api/github`);
-      const repos = await data.json();
-      setRepos(repos.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch(
+          `${clientEnv.NEXT_PUBLIC_BASE_URL}/api/github`
+        );
+        const reposArr = await data.json();
+        setRepos(reposArr.data);
+        ToastHelper.success('Successfully fetched data');
+      } catch (error) {
+        ToastHelper.error('Error fetching data');
+      }
+    };
     fetchData().then();
   }, []);
 
   const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
     try {
+      RepoAddSchema.parse({ url });
       setLoader(true);
       const res = await fetch(`${clientEnv.NEXT_PUBLIC_BASE_URL}/api/github`, {
         method: 'POST',
@@ -33,15 +42,23 @@ const DashboardContribution = () => {
         },
         body: JSON.stringify({ url }),
       });
-
       const output = await res.json();
-      console.log('final output is : ', output);
-      if (output) {
+      if (output.success === true) {
         setLoader(false);
         setOutput(output);
+        ToastHelper.success('Repo added successfully');
+      } else {
+        setLoader(false);
+        ToastHelper.error(output.data || 'Repo add failed');
       }
-    } catch (error) {
-      console.error(error);
+    } catch (e) {
+      if (e instanceof z.ZodError) {
+        e.errors.forEach(err => {
+          if (err.path[0] === 'url') {
+            ToastHelper.error(err.message);
+          }
+        });
+      }
     }
   };
   return (
@@ -66,11 +83,29 @@ const DashboardContribution = () => {
               <pre>{output}</pre>
               <div className="text-white mt-6">
                 {' '}
-                data : {JSON.stringify(repos)}
+                data : {JSON.stringify(repos[0])}
               </div>
             </form>
           </div>
         </div>
+      </div>
+
+      <div className="mx-auto max-w-screen-xl mt-6 grid grid-cols-2">
+        {repos.map((repo, i) => (
+          <div
+            key={i}
+            className="p-6 border rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+          >
+            <h4 className="text-white font-bold text-2xl mb-2">{repo.name}</h4>
+            <p className="text-white font-normal">{repo.description}</p>
+            <div className="mt-3">
+              <span className="text-white mr-2">
+                Forks: {repo.stats.forks},
+              </span>
+              <span className="text-white mr-1">Stars: {repo.stats.stars}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
