cea095e6cbad1139c35b901090fcf8b4e2750ea7
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-16 01:28:03 +0600

    update getRepos  service

--- src/services/backend/github/getRepos.ts
+++ src/services/backend/github/getRepos.ts
@@ -1,13 +1,14 @@
 import connectMongo from '@/config/mongo';
 import RepoModel from '@/models/Repo';
 import logger from '@/config/logger';
-import { SetRepoData } from '@/interface/opensource';
 
 export const getRepos = async () => {
   await connectMongo();
   try {
     return await RepoModel.find({});
   } catch (e) {
-    logger.error(e, 'failed to get repos');
+    const error = 'failed to get repos';
+    logger.error(e, error);
+    throw new Error(error);
   }
 };
