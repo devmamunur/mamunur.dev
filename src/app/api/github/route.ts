04ddd9229c21312c0bbdce2424e37a992209e5fe
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-15 11:01:28 +0600

    Made GET route for getting repos

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { addGitHubRepo } from '@/services/backend/github/addGitHubRepo';
 import { serverEnv } from '@/config/schemas/serverSchema';
+import { getRepos } from '@/services/backend/github/getRepos';
 
 export async function POST(req: Request) {
   try {
@@ -11,6 +12,18 @@ export async function POST(req: Request) {
       reqBody.url
     );
 
+    return NextResponse.json({ success: true, data: repo }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, data: (error as Error).message },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    const repo = await getRepos();
     return NextResponse.json({ success: true, data: repo }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
