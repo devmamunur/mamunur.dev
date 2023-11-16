6f6c85b5d860f0022f32619c99293cfe28f20684
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-17 02:22:06 +0600

    update contribution page route

---
+++ src/app/dashboard/opensource/contributions/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import OpensourceContribution from '@/components/Dashboard/Opensource/Contribution/OpensourceContribution';
+
+const OpensourceContributionsPage = () => {
+  return (
+    <>
+      <OpensourceContribution />
+    </>
+  );
+};
+
+export default OpensourceContributionsPage;
