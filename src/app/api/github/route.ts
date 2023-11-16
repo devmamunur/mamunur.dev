f509ea113b8c1fdce0593684c237d739a6974a83
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-17 02:22:32 +0600

    update github api route remove unused code

--- src/app/api/github/route.ts
+++ src/app/api/github/route.ts
@@ -1,16 +1,12 @@
 import { NextResponse } from 'next/server';
-import { addGitHubRepo } from '@/services/backend/github/addGitHubRepo';
+import { addGitHubRepo } from '@/services/api/github/addGitHubRepo';
 import { serverEnv } from '@/config/schemas/serverSchema';
-import { getRepos } from '@/services/backend/github/getRepos';
+import { getRepos } from '@/services/api/github/getRepos';
 
 export async function POST(req: Request) {
   try {
     const reqBody = await req.json();
-    const repo = await addGitHubRepo(
-      req,
-      serverEnv.GITHUB_USERNAME,
-      reqBody.url
-    );
+    const repo = await addGitHubRepo(serverEnv.GITHUB_USERNAME, reqBody);
 
     return NextResponse.json({ success: true, data: repo }, { status: 200 });
   } catch (error) {
