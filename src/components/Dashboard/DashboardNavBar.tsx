0ccd5190a92b55d3ac4587dbe1bb945cdeeee380
Mamunur Rashid <devmamunur1@gmail.com>
2023-11-17 02:21:33 +0600

    Update nav page link

--- src/components/Dashboard/DashboardNavBar.tsx
+++ src/components/Dashboard/DashboardNavBar.tsx
@@ -8,13 +8,13 @@ const DashboardNavBar: React.FC = () => {
     <>
       <nav className="max-w-screen-xl mx-auto mt-6">
         <div className=" flex flex-wrap items-center justify-between  p-4  border-gray-200 rounded-lg dark:bg-slate-800 relative">
-          <Link href={`/`} className="flex items-center">
+          <Link href={'/'} className="flex items-center">
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               @devmamunur
             </span>
           </Link>
           <div className="flex md:order-2">
-            <Link href={`https://github.com/devmamunur/mamunur.dev`}>
+            <Link href={'https://github.com/devmamunur/mamunur.dev'}>
               <button
                 type="button"
                 className="text-gray-900 bg-transparent border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-4 py-2 mr-2 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
@@ -39,7 +39,7 @@ const DashboardNavBar: React.FC = () => {
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4  bg-transparent rounded-lg  md:flex-row md:space-x-8 md:mt-0 ">
               <li>
                 <Link
-                  href={`/dashboard/home`}
+                  href={'/dashboard/home'}
                   className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-sky-500"
                   aria-current="page"
                 >
@@ -48,7 +48,7 @@ const DashboardNavBar: React.FC = () => {
               </li>
               <li>
                 <Link
-                  href={`/dashboard/opensource-projects`}
+                  href={'/dashboard/opensource/projects'}
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-sky-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Opensource Projects
@@ -56,7 +56,7 @@ const DashboardNavBar: React.FC = () => {
               </li>
               <li>
                 <Link
-                  href={`/dashboard/opensource-contributions`}
+                  href={'/dashboard/opensource/contributions'}
                   className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-sky-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Opensource Contributions
